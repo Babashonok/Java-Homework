9e20869f9f7f188673a4ce5ed52aec3f67ea68a1
Babashonok <Babashonok.ab@gmail.com>
2016-10-01 20:31:28 +0300

    Add files via upload

---
+++ SayMeYourName.java
@@ -0,0 +1,15 @@
+public class SayMeYourName {
+    //output string name from command line 
+    public static void main(String [] args) {
+        if(args.length == 0) {
+            System.out.println(" Please,enter your name ");
+        }
+        else if(args.length > 1) {
+            System.out.println("Hello, " + args[0] + ", no need to give additional information");
+        }
+        else {
+            System.out.println("Hello, " + args[0]);
+        }
+    }
+}
+		
