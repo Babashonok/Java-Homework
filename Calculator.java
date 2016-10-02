12799c8f80361b9b461d7dcb534407f91cc89ffa
Alexey Babak <Babashonok.ab@gmail.com>
2016-10-02 22:07:06 +0300

    Update Calculator.java
    method comment  to javadoc
    put "} else if" and "} else" structures in 1 line

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,7 @@
  */
 public class Calculator {
 
-   //check possibility of the "String -> Double" conversion
+   /** check possibility of the "String -> Double" conversion */
    public static boolean checkString(String string) {
       try {
          Double.parseDouble(string);
@@ -14,15 +14,13 @@ public class Calculator {
       return true;
    }
 
-   //make 4 simpliest math methods(+-*/) with 2 numbers from command line
+   /** make 4 simpliest math methods(+*-/) with 2 numbers from command line*/
    public static void main(String [] args) {
       if(args.length != 2) {
          System.out.println("Try to input 2 numbers");
-      }
-      else if(!checkString(args[0]) || !checkString(args[1])) {
+      } else if(!checkString(args[0]) || !checkString(args[1])) {
          System.out.println("Try to input 2 NUMBERS");
-      }
-      else {
+      } else {
          double num1 = Double.parseDouble(args[0]);
          double num2 = Double.parseDouble(args[1]);
          double sum = num1 + num2;
@@ -31,8 +29,7 @@ public class Calculator {
          System.out.println("Multiplier = " + num1*num2);
          if(num2 == 0) {
             System.out.println("Dividind by zero");
-         }
-         else System.out.println("Division = " + num1/num2);			
+         } else System.out.println("Division = " + num1/num2);			
       }
    }
 }
