d862e8d0bc259562e442fa5649dcc6a9e0477c62
Alexey Babak <Babashonok.ab@gmail.com>
2016-10-02 22:05:05 +0300

    Update SayMeYourName.java
    method comment  to javadoc
    put "} esle if" and "} else" structures in 1 line
    rework print from "Please,enter your name" to "Error, try to enter your name next time"

--- SayMeYourName.java
+++ SayMeYourName.java
@@ -3,15 +3,13 @@
 * Babak Alexey
 */
 public class SayMeYourName {
-   //output string name from command line 
+   /** output string name from command line */
    public static void main(String [] args) {
       if(args.length == 0) {
-         System.out.println(" Please,enter your name ");
-      }
-      else if(args.length > 1) {
+         System.out.println(" Error, try to enter your name next time");
+      } else if(args.length > 1) {
          System.out.println("Hello, " + args[0] + ", no need to give additional information");
-      }
-      else {
+      } else {
          System.out.println("Hello, " + args[0]);
       }
    }
