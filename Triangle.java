f9438892356a0a4ea63001038cc6f5b02dca9e21
Alexey Babak <Babashonok.ab@gmail.com>
2016-10-02 22:25:44 +0300

    Add files via upload
    method comment  to javadoc
    put "} esle if" and "} else" structures in 1 line
    create method checkType
    add System.exit() to terminate wrong inputs

--- Triangle.java
+++ Triangle.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
  * Babak Alexey
  */
 public class Triangle {
-   //find type of the triangle
+   /** find equalites between sides */
+   public static void checkType(double num0 , double num1 , double num2) {
+      if(num0 == num1 && num0 == num2) {
+         System.out.println("it is equilateral triangle");
+      } else if(num0 != num1 && num0 != num2 && num1 != num2) {
+         System.out.println("it is normal random triangle");
+      } else System.out.println("it is isosceles triangle");
+   }
+   /**find type of the triangle */
    public static void main(String [] args) {
       Scanner var = new Scanner(System.in);
       System.out.println("input 3 numeric lengths");
@@ -13,26 +21,18 @@ public class Triangle {
 	for(int i = 0 ; i < numOfSizes ; i++) {	  
          if(var.hasNextDouble() ) {  
             side[i]= var.nextDouble();      
-         } 
-         else {
+         } else {
             System.out.println("wrong input, try to input 3 numbers");
-            break;
+            System.exit(0);
          }
          if(side[i] <= 0) {
             System.out.println("wrong input, try to input positive numbers");
-            break;
+            System.exit(0);
          }
       }
       if((side[0] >= side[1]+side[2]) ||(side[1] >= side[0]+side[2])||(side[2] >= side[1]+side[0])) {
          System.out.println("one side larger than sum of others, try again");
-      }
-      //check type
-	else if(side[0] == side[1] &&	side[0] == side[2]) {
-         System.out.println("it is equilateral triangle");
-      }
-      else if(side[0] != side[1] && side[0] != side[2] && side[1] != side[2]) {
-         System.out.println("it is normal random triangle");
-      }
-      else	System.out.println("it is isosceles triangle");
+         System.exit(0);
+      } else checkType(side[0] , side[1], side[2]);     	
    }      
 }
