a714235310be51ba31e3831a553c856e2ca3d0ad
Alexey Babak <Babashonok.ab@gmail.com>
2016-10-03 15:32:35 +0300

    Update CommandLineParams.java
    another rework of javadoc

--- CommandLineParams.java
+++ CommandLineParams.java
@@ -1,11 +1,11 @@
 /**
-* output parameters from command line in reverse order 
-* @author Babak Alexey
-*/
+ * output parameters from command line in reverse order 
+ * @author Babak Alexey
+ */
 public class CommandLineParams {
    /**
-   *Output parameters from command line in reverse order. 
-   */
+    *Output parameters from command line in reverse order. 
+    */
    public static void main(String[] args) {
       for (int i = args.length -1; i >= 0 ; i--) {
          System.out.println("Variable " + i + " = " + args[i]);
