00d5836e5c20cd1e2d88cae0b2579dcd99d2392f
Alexey Babak <Babashonok.ab@gmail.com>
2016-10-03 15:00:50 +0300

    Update SayMeYourName.java
    add space between if and (

--- SayMeYourName.java
+++ SayMeYourName.java
@@ -7,9 +7,9 @@ public class SayMeYourName {
    *output string name from command line 
    */
    public static void main(String [] args) {
-      if(args.length == 0) {
+      if (args.length == 0) {
          System.out.println(" Error, try to enter your name next time");
-      } else if(args.length > 1) {
+      } else if (args.length > 1) {
          System.out.println("Hello, " + args[0] + ", no need to give additional information");
       } else {
          System.out.println("Hello, " + args[0]);
