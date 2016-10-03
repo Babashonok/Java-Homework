5eb262f5a31927bad0eb107a1aff3ef1e76d722b
Alexey Babak <Babashonok.ab@gmail.com>
2016-10-03 14:59:45 +0300

    Update CommandLineParams.java
    add space between for and (

--- CommandLineParams.java
+++ CommandLineParams.java
@@ -7,7 +7,7 @@ public class CommandLineParams {
    *Output parameters from command line in reverse order. 
    */
    public static void main(String[] args) {
-      for(int i = args.length -1; i >= 0 ; i--) {
+      for (int i = args.length -1; i >= 0 ; i--) {
          System.out.println("Variable " + i + " = " + args[i]);
       }
    }
