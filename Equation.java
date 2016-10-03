7eaab3ce0cbb7d9bca965323e8f01cfed419997e
Alexey Babak <Babashonok.ab@gmail.com>
2016-10-03 22:57:34 +0400

    Update Equation.java
    27: change comment
    39: rework dividind by zero
    add space between if and (

--- Equation.java
+++ Equation.java
@@ -23,16 +23,20 @@ public class Equation {
    public static void main(String [] args) {
       if (args.length != 3) {
          System.out.println("Try to input 3 numbers ( a,b,c)");
+         System.exit(0);
       } else if (!checkString(args[0]) || !checkString(args[1])|| !checkString(args[2])) {
-         System.out.println("Try to input 3 NUMBERS");
+         System.out.println("Try to input 3 numbers, not letters or other symbols");
+         System.exit(0);
       }
       double a = Double.parseDouble(args[0]);
       double b = Double.parseDouble(args[1]);
       double c = Double.parseDouble(args[2]);      
       double discr = pow(b,2.0) - 4*a*c; //find discriminant
-      if(discr < 0) {
+      double test = 1./a;
+      if (discr < 0) {
          System.out.println("Negative discriminant");
-      } else if (a != 0){
+         System.exit(0);
+      } else if (!Double.isInfinite(test) || !Double.isInfinite(-test)){
          double x1 = (-b + sqrt(discr))/(2*a);
          double x2 = (-b - sqrt(discr))/(2*a);	
          System.out.println("x1 = " + x1 +" x2 = " + x2);
