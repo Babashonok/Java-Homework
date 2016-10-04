953e18364506f7080e1edf19a99bb14ee97aa3b2
Alexey Babak <Babashonok.ab@gmail.com>
2016-10-04 14:30:00 +0400

    Add files via upload

--- Triangle.java
+++ Triangle.java
@@ -4,11 +4,27 @@ import java.util.Scanner;
  * @author Babak Alexey
  */
 public class Triangle {
+   /**
+    * retype Scanner to Double and return the array
+    * @param numOfSizes  number of the triangle sides 
+    */
+   public static Double[] retype(int numOfSizes) {
+      Scanner var = new Scanner(System.in);
+      Double [] side = new Double[numOfSizes];
+      for (int i = 0 ; i < numOfSizes ; i++) {	  
+         if (var.hasNextDouble() ) {  
+            side[i]= var.nextDouble();      
+         } else {
+            side[0]= Double.NEGATIVE_INFINITY;
+         }
+      } 
+      return side;
+   }
    /** 
     * find equalites between sides
-    * @param num0
-    * @param num1
-    * @param num2 all 3 are represent triangle sides
+    * @param num0  represent triangle side
+    * @param num1  represent triangle side
+    * @param num2  represent triangle side
     */
    public static void checkType(Double num0 , Double num1 , Double num2) {
       if (num0.equals(num1) && num0.equals(num2)) {
@@ -24,22 +40,20 @@ public class Triangle {
       Scanner var = new Scanner(System.in);
       System.out.println("input 3 numeric lengths");
       int numOfSizes = 3; 
-      Double [] side = new Double[numOfSizes];
-         for (int i = 0 ; i < numOfSizes ; i++) {	  
-         if (var.hasNextDouble() ) {  
-            side[i]= var.nextDouble();      
-         } else {
+      Double [] side = retype(numOfSizes);     
+      if (Double.isInfinite(-side[0])) {
             System.out.println("wrong input, try to input 3 numbers");
-            System.exit(0);
-         }
+            System.exit(1);
+      }
+      for (int i = 0 ; i < numOfSizes ; i++) {	  
          if (side[i] <= 0) {
             System.out.println("wrong input, try to input positive numbers");
-            System.exit(0);
+            System.exit(1);
          }
       }
       if ((side[0] >= side[1]+side[2]) ||(side[1] >= side[0]+side[2])||(side[2] >= side[1]+side[0])) {
          System.out.println("one side larger than sum of others, try again");
-         System.exit(0);
+         System.exit(1);
       } else {
 	 checkType(side[0] , side[1], side[2]);  
       }
