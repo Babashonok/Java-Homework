fb0d725886906864b960798e6ec310cd7907ba63
Alexey Babak <Babashonok.ab@gmail.com>
2016-10-26 15:19:04 +0200

    Maven Project

---
+++ Transport/java/transport/Bicycle.java
@@ -0,0 +1,36 @@
+package tat.bsu.transport;
+
+import tat.bsu.route.DistanceCounter;
+
+/**
+ * Class that realize bicycle way of passing the path
+ */
+public class Bicycle implements Moveable {
+
+    private double speed = 30;
+
+    /**
+     * interface method that gives object an ability to determine oneself speed
+     * @return
+     */
+    public double getSpeed() {
+        return speed;
+    }
+    /**
+     * find spent consumption in the path
+     * @param counter
+     * @return
+     */
+    public double findPathTime(DistanceCounter counter){
+        return counter.getWholeDistance() / speed;
+    }
+    /**
+     * interface method that gives object an ability to output characteristics about movement
+     * @param counter
+     * @return
+     */
+    public String getInfoAboutMoves(DistanceCounter counter) {
+        return "Bicycle : path time = " + findPathTime(counter) + " | cost per person = 0";
+
+    }
+}
---
+++ Transport/java/transport/Car.java
@@ -0,0 +1,35 @@
+package tat.bsu.transport;
+
+import tat.bsu.route.DistanceCounter;
+
+/**
+ * Class that realize car way of passing the path
+ */
+public class Car extends Vehicle implements Moveable {
+
+    private double speed;
+    private double consumption100Km ;
+    private int numberOfPassangers ;
+
+    /**
+     * implement constructor of the parent clas Vehicle
+     * @param speed
+     * @param consumption100Km
+     * @param numberOfPassangers
+     */
+    public Car(double speed, double consumption100Km, int numberOfPassangers) {
+        super(speed, consumption100Km, numberOfPassangers);
+    }
+
+    /**
+     * override method from Vehicle that gives object an ability to output characteristics about movement
+     * @param counter
+     * @return
+     */
+    @Override
+    public String getInfoAboutMoves(DistanceCounter counter) {
+        return "Car : path time = " + findPathTime(counter) + " | cost per person = "
+                + findConsumption(counter) / getNumberOfPassangers();
+    }
+
+}
