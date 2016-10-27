b2f8ddd7df42b42989fd543c5486c0ae8617f912
Alexey Babak <Babashonok.ab@gmail.com>
2016-10-27 14:02:16 +0200

    Maven Project

--- Transport/java/Main.java
+++ Transport/java/Main.java
@@ -1,4 +1,4 @@
-import tat.bsu.route.ObsessionExceptionHandler;
+import tat.bsu.route.ObsessionHandler;
 import tat.bsu.route.DistanceCounter;
 import tat.bsu.route.Path;
 import tat.bsu.transport.*;
@@ -16,36 +16,61 @@ public class Main {
             + File.separator + "java" + File.separator + "coord.txt" ;
 
     /**
+     * list of constant parameters of all transports in the program
+     */
+    public static final double carSpeed = 90;
+    public static final double carConsumption100Km = 6.5;
+    public static final int carNumberOfPassangers = 4;
+
+    public static final double busSpeed = 70;
+    public static final double busConsumption100Km = 20;
+    public static final int busNumberOfPassangers = 40;
+
+    public static final double bicycleSpeed = 30;
+    public static final double footworkSpeed = 5;
+
+    /**
+     * find and get file with coordinates list
+     * @return
+     * @throws FileNotFoundException
+     */
+    public static Scanner getOutputFile() throws FileNotFoundException {
+        String curDir = System.getProperty("user.dir");
+        Scanner output = new Scanner(new File(curDir + pathToFile));
+        return  output;
+    }
+
+    /**
      * create list of transpot ways to pass the path
      * @return
      */
     public static TransportList createTransportList() {
-        TransportList transportList = new TransportList().add(new Car(90, 6.5, 4))
-                                                         .add(new Bus(70, 20, 40))
-                                                         .add(new Bicycle())
-                                                         .add(new Footwork());
+        TransportList transportList = new TransportList().add(new Car(carSpeed, carConsumption100Km, carNumberOfPassangers))
+                                                         .add(new Bus(busSpeed, busConsumption100Km, busNumberOfPassangers))
+                                                         .add(new Bicycle(bicycleSpeed))
+                                                         .add(new Footwork(footworkSpeed));
         return transportList;
     }
     /**
      * Entry point of the program and point of handling exceptions
      * @param args
-     * @throws FileNotFoundException
      * @throws NumberFormatException
      */
-    public static void main ( String [] args ) throws FileNotFoundException, NumberFormatException {
-        String curDir = System.getProperty("user.dir");
-        Path path =new Path();
-        TransportList transportList = createTransportList();
+    public static void main ( String [] args ) throws NumberFormatException {
         try {
-            Scanner output = new Scanner(new File(curDir + pathToFile));
-            ObsessionExceptionHandler handler = new ObsessionExceptionHandler();
-            path.createPath(output);
+            TransportList transportList = createTransportList();
+            Path path =new Path();
+            path.createPath(getOutputFile());
+            DistanceCounter counter = new DistanceCounter(path);
+
+            ObsessionHandler handler = new ObsessionHandler();
             if (handler.checkPathObsession(path.getCertainCheckpoint(0), path.getCertainCheckpoint(path.getAmountOfCheckpoints() - 1))) {
                 System.out.println("Obsession: begin and end has equal coordinates ");
-                System.exit(0);
+                System.exit(1);
             }
-            DistanceCounter counter = new DistanceCounter(path);
+
             transportList.outputStat(counter);
+
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
             System.exit(1);
