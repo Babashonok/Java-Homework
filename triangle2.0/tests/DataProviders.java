e5d1a0227b201c19625332734880bf01e6121744
Alexey Babak <Babashonok.ab@gmail.com>
2016-11-10 00:59:04 +0300

    XML File Reader

--- triangle2.0/tests/DataProviders.java
+++ triangle2.0/tests/DataProviders.java
@@ -1,45 +1,89 @@
 package triangle;
 
 import org.testng.annotations.DataProvider;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
 
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 import java.math.BigDecimal;
+
 
 /**
  * DataProviders
  */
 public class DataProviders {
 
-    @DataProvider(name = "IsoscelesTriangle")
-     public static Object[][] getIsoscelesTriangleVariables() {
-        return new Object[][]{
-                {BigDecimal.valueOf(3), BigDecimal.valueOf(2), BigDecimal.valueOf(2)},
-                {BigDecimal.valueOf(2), BigDecimal.valueOf(3), BigDecimal.valueOf(2)},
-                {BigDecimal.valueOf(2), BigDecimal.valueOf(2), BigDecimal.valueOf(3)},
-        };
+    private static final String SIDE_A = "side_a";
+    private static final String SIDE_B = "side_b";
+    private static final String SIDE_C = "side_c";
+    private static final String MESSAGE = "typeMessage";
+    private static final String STRING = "string";
+
+    private static final String TRIANGLE = "triangle";
+    private static final String NONEXIST = "nonExist";
+    private static final String INVALID = "invalid";
+
+    private static final String filePath = "data.xml";
+
+    /**
+     * removal of duplicate code from data providers
+     * @return
+     * @throws Exception
+     */
+    public static Document getDocument() throws Exception {
+        File file = new File(filePath);
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        return builder.parse(file);
     }
-    @DataProvider(name = "CommonTriangle")
-    public static Object[][] getCommonTriangleVariables() {
-        return new Object[][]{
-                {BigDecimal.valueOf(3), BigDecimal.valueOf(2), BigDecimal.valueOf(4)},
-                {BigDecimal.valueOf(4), BigDecimal.valueOf(3), BigDecimal.valueOf(2)},
-                {BigDecimal.valueOf(2), BigDecimal.valueOf(4), BigDecimal.valueOf(3)},
-        };
+
+    @DataProvider (name = "valid triangles")
+    public static Object[][] getEquilateralTriangles() throws Throwable {
+        Document document = getDocument();
+        NodeList nodes = document.getElementsByTagName(TRIANGLE);
+        Object[][] triangles = new Object[nodes.getLength()][];
+        for (int i = 0; i < nodes.getLength(); i++) {
+            NamedNodeMap attrs = nodes.item(i).getAttributes();
+            triangles[i] = new Object[] {
+                       new Triangle(new BigDecimal[] {
+                                    new BigDecimal(attrs.getNamedItem(SIDE_A).getNodeValue()),
+                                    new BigDecimal(attrs.getNamedItem(SIDE_B).getNodeValue()),
+                                    new BigDecimal(attrs.getNamedItem(SIDE_C).getNodeValue())}),
+                    attrs.getNamedItem(MESSAGE).getNodeValue()
+                 };
+        }
+    return triangles;
     }
-    @DataProvider(name = "NonExistTriangle")
-    public static Object[][] getNonExistTriangleVariables() {
-        return new Object[][]{
-                {BigDecimal.valueOf(1), BigDecimal.valueOf(2), BigDecimal.valueOf(3)},
-                {BigDecimal.valueOf(2), BigDecimal.valueOf(3), BigDecimal.valueOf(1)},
-                {BigDecimal.valueOf(3), BigDecimal.valueOf(1), BigDecimal.valueOf(2)},
-                {BigDecimal.valueOf(1), BigDecimal.valueOf(2), BigDecimal.valueOf(100)},
-                {BigDecimal.valueOf(2), BigDecimal.valueOf(100), BigDecimal.valueOf(1)},
-                {BigDecimal.valueOf(100), BigDecimal.valueOf(1), BigDecimal.valueOf(2)}
-        };
+    @DataProvider (name = "nonExist triangles")
+    public static Object[][] getNonExistTriangles() throws Exception {
+        Document document = getDocument();
+        NodeList nodes = document.getElementsByTagName(NONEXIST);
+        Object[][] triangles = new Object[nodes.getLength()][];
+        for (int i = 0; i < nodes.getLength(); i++) {
+            NamedNodeMap attrs = nodes.item(i).getAttributes();
+            triangles[i] = new Object[] {
+                    new BigDecimal(attrs.getNamedItem(SIDE_A).getNodeValue()),
+                    new BigDecimal(attrs.getNamedItem(SIDE_B).getNodeValue()),
+                    new BigDecimal(attrs.getNamedItem(SIDE_C).getNodeValue())
+            };
+        }
+        return triangles;
     }
     @DataProvider(name = "InvalidInput")
-    public static Object[][] getInvalidInputVariables() {
-        return new Object[][]{
-                {BigDecimal.valueOf(0)},{BigDecimal.valueOf(-1)},{null}
-        };
+    public static Object[][] getInvalidInputVariables() throws Exception {
+        Document document = getDocument();
+        NodeList nodes = document.getElementsByTagName(INVALID);
+        Object[][] triangles = new Object[nodes.getLength()][];
+        for (int i = 0 ; i < nodes.getLength() ; i++) {
+            NamedNodeMap attrs = nodes.item(i).getAttributes();
+            triangles[i] = new Object[] {
+                    attrs.getNamedItem(STRING) == null ? null : new BigDecimal(attrs.getNamedItem(STRING).getNodeValue())
+            };
+        }
+        return triangles;
     }
 }
--- triangle2.0/tests/TriangleTest.java
+++ triangle2.0/tests/TriangleTest.java
@@ -1,34 +1,18 @@
--package triangle;
- -
- -import org.testng.annotations.Test;
- -
- -import java.math.BigDecimal;
- -
- -import static org.testng.Assert.*;
- -
- -/**
- - * test Class Triangle
- - */
- -public class TriangleTest {
- -
- -    @Test(dataProvider = "IsoscelesTriangle", dataProviderClass = DataProviders.class)
- -    public void isoscelesTriangleTestofMethodGetTriangleType(BigDecimal side1, BigDecimal side2, BigDecimal side3) throws Exception {
- -        Triangle triangle = new Triangle(new BigDecimal[]{side1, side2, side3});
- -        String message = triangle.getTriangleType();
- -        assertEquals(message, "This triangle is isosceles");
- -    }
- -    @Test(dataProvider = "CommonTriangle", dataProviderClass = DataProviders.class)
- -    public void commonTriangleTestofMethodGetTriangleType(BigDecimal side1, BigDecimal side2, BigDecimal side3) throws Exception {
- -        Triangle triangle = new Triangle(new BigDecimal[]{side1, side2, side3});
- -        String message = triangle.getTriangleType();
- -        assertEquals(message, "This triangle is common");
- -    }
- -    @Test
- -    public void equilateralTriangleTestofMethodGetTriangleType() throws Exception {
- -        Triangle triangle = new Triangle(new BigDecimal[]{BigDecimal.valueOf(5), BigDecimal.valueOf(5), BigDecimal.valueOf(5)});
- -        String message = triangle.getTriangleType();
- -        assertEquals(message, "This triangle is equilateral so as isosceles");
- -    }
- -
- -
- -}
+package triangle;
+
+import org.testng.annotations.Test;
+
+
+
+import static org.testng.Assert.*;
+
+/**
+ * test Class Triangle
+ */
+public class TriangleTest {
+
+    @Test(dataProvider = "valid triangles", dataProviderClass = DataProviders.class)
+    public void testValidTrianglesType(Triangle triangle, String message) throws Exception {
+        assertEquals(triangle.getTriangleType(), message);
+    }
+}
