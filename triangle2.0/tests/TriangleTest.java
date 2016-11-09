6589d6ece68cb18343e40b51a7707eb25228b7d7
Alexey Babak <Babashonok.ab@gmail.com>
2016-11-10 00:37:23 +0300

    Update TriangleTest.java

--- triangle2.0/tests/TriangleTest.java
+++ triangle2.0/tests/TriangleTest.java
@@ -1,1 +1,15 @@
+package triangle;
 
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+/**
+ * test Class Triangle
+ */
+public class TriangleTest {
+
+    @Test(dataProvider = "valid triangles", dataProviderClass = DataProviders.class)
+    public void testValidTrianglesType(Triangle triangle, String message) throws Exception {
+        assertEquals(triangle.getTriangleType(), message);
+    }
+}
