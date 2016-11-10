0f9b0165fbf279625b47ee7a439dc26928343509
Alexey Babak <Babashonok.ab@gmail.com>
2016-11-10 14:53:18 +0300

    XML File Reader

--- triangle2.0/tests/DataProviders.java
+++ triangle2.0/tests/DataProviders.java
@@ -1,14 +1,7 @@
 package triangle;
 
 import org.testng.annotations.DataProvider;
-import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.NodeList;
-
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
 import java.math.BigDecimal;
 
 
@@ -27,27 +20,13 @@ public class DataProviders {
     private static final String NONEXIST = "nonExist";
     private static final String INVALID = "invalid";
 
-    private static final String filePath = "data.xml";
-
-    /**
-     * removal of duplicate code from data providers
-     * @return
-     * @throws Exception
-     */
-    public static Document getDocument() throws Exception {
-        File file = new File(filePath);
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        return builder.parse(file);
-    }
 
     @DataProvider (name = "valid triangles")
     public static Object[][] getEquilateralTriangles() throws Throwable {
-        Document document = getDocument();
-        NodeList nodes = document.getElementsByTagName(TRIANGLE);
-        Object[][] triangles = new Object[nodes.getLength()][];
-        for (int i = 0; i < nodes.getLength(); i++) {
-            NamedNodeMap attrs = nodes.item(i).getAttributes();
+        DuplicateCode code = new DuplicateCode(TRIANGLE);
+        Object[][] triangles = code.getMatrixOfObjects();
+        for (int i = 0; i <code.getListOfNodes().getLength(); i++) {
+            NamedNodeMap attrs = code.getListOfNodes().item(i).getAttributes();
             triangles[i] = new Object[] {
                        new Triangle(new BigDecimal[] {
                                     new BigDecimal(attrs.getNamedItem(SIDE_A).getNodeValue()),
@@ -60,11 +39,10 @@ public class DataProviders {
     }
     @DataProvider (name = "nonExist triangles")
     public static Object[][] getNonExistTriangles() throws Exception {
-        Document document = getDocument();
-        NodeList nodes = document.getElementsByTagName(NONEXIST);
-        Object[][] triangles = new Object[nodes.getLength()][];
-        for (int i = 0; i < nodes.getLength(); i++) {
-            NamedNodeMap attrs = nodes.item(i).getAttributes();
+        DuplicateCode code = new DuplicateCode(NONEXIST);
+        Object[][] triangles = code.getMatrixOfObjects();
+        for (int i = 0; i < code.getListOfNodes().getLength(); i++) {
+            NamedNodeMap attrs = code.getListOfNodes().item(i).getAttributes();
             triangles[i] = new Object[] {
                     new BigDecimal(attrs.getNamedItem(SIDE_A).getNodeValue()),
                     new BigDecimal(attrs.getNamedItem(SIDE_B).getNodeValue()),
@@ -75,11 +53,10 @@ public class DataProviders {
     }
     @DataProvider(name = "InvalidInput")
     public static Object[][] getInvalidInputVariables() throws Exception {
-        Document document = getDocument();
-        NodeList nodes = document.getElementsByTagName(INVALID);
-        Object[][] triangles = new Object[nodes.getLength()][];
-        for (int i = 0 ; i < nodes.getLength() ; i++) {
-            NamedNodeMap attrs = nodes.item(i).getAttributes();
+        DuplicateCode code = new DuplicateCode(INVALID);
+        Object[][] triangles = code.getMatrixOfObjects();
+        for (int i = 0 ; i < code.getListOfNodes().getLength() ; i++) {
+            NamedNodeMap attrs = code.getListOfNodes().item(i).getAttributes();
             triangles[i] = new Object[] {
                     attrs.getNamedItem(STRING) == null ? null : new BigDecimal(attrs.getNamedItem(STRING).getNodeValue())
             };
---
+++ triangle2.0/tests/DuplicateCode.java
@@ -0,0 +1,51 @@
+package triangle;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
+
+/**
+ * Entity to work with code duplication in data providers
+ */
+public class DuplicateCode {
+
+    private static final String filePath = "data.xml";
+
+
+    private NodeList nodes;
+
+    /**
+     * read data from XML file
+     * create list of nodes with equal tag name
+     * @param message tag name
+     * @throws Exception
+     */
+    public DuplicateCode(String message) throws Exception {
+        File file = new File(filePath);
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(file);
+        nodes = document.getElementsByTagName(message);
+
+    }
+
+    /**
+     * return empty mathix of objects
+     * number of string = number of elements with equal tag name in XML file
+     * @return
+     */
+    public Object [][] getMatrixOfObjects() {
+        return new Object[nodes.getLength()][];
+    }
+
+    /**
+     * return list of nodes with equal tag name
+     * @return
+     */
+    public NodeList getListOfNodes() {
+        return nodes;
+    }
+}
